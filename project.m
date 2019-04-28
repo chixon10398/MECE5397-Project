ba8438365f4efd5a0ea256866242a05c17ab57bb
chixon10398 <49700356+chixon10398@users.noreply.github.com>
2019-04-27 23:45:56 -0500

    Update project.m
    Fixed the way the code solves for F values, started working on the ghost node portion.

--- project.m
+++ project.m
@@ -8,14 +8,31 @@ b = 2*pi;
 %discretize the problem
 n = 100; %number of spaces
 h = b/n; %distance between each position
-x = h:h:b;
+x = 0:h:b;
 y = x;
 %create the functions
 fb = y.*(b-y);
 gb = ((b-y).^2).*cos(pi*y/b);
-F = sin(pi*x/b).*cos((pi/2)*(2*y/b+1));
+z = 1;
+F = zeros(1,(n-1)^2); %create vector for F values
+for j = h:h:b-h
+    for i = h:h:b-h
+        F(z) = sin(pi*i/b)*cos((pi/2)*(2*j/b+1));
+        z = z+1;
+    end
+end
 %Boundary conditions
 u_x0 = fb;
 u_xb = gb;
 u_y0 = fb(1)+(x/b).*(gb(1)-fb(1));
+%Build the vector for U values
+U = zeros((n-1)^2,1);
+U_D = ones(n-1,1); %assign a value for the Dirchielt boundary conditions
+
 %ghost node
+z=1;
+for i = h:h:b-h
+    F_D(z) = sin(pi*i/b)*cos(pi/2);
+    z=z+1;
+end
+
