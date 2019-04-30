2e25336d0ab3fa465caf49a537dea3dc032a1eca
chixon10398 <49700356+chixon10398@users.noreply.github.com>
2019-04-29 22:14:38 -0500

    Update project.m
    Added the loop that will solve for Gauss-Siedel method with ghost node for one iteration

--- project.m
+++ project.m
@@ -6,33 +6,49 @@ clc
 a = 0;
 b = 2*pi;
 %discretize the problem
-n = 100; %number of spaces
+n = 6; %number of steps to b
 h = b/n; %distance between each position
 x = 0:h:b;
 y = x;
 %create the functions
 fb = y.*(b-y);
 gb = ((b-y).^2).*cos(pi*y/b);
-z = 1;
-F = zeros(1,(n-1)^2); %create vector for F values
-for j = h:h:b-h
-    for i = h:h:b-h
-        F(z) = sin(pi*i/b)*cos((pi/2)*(2*j/b+1));
-        z = z+1;
+F = zeros(n,n-1); %create vector for F values
+U_n = zeros(n,n-1); %U for n
+U_n1 = zeros(n,n-1); % U for n+1
+%create the elements for F
+for j = 1:n
+    for i = 1:n-1
+        F(j,i) = sin(pi*i/b)*cos((pi/2)*(2*j/b+1));
     end
 end
+
 %Boundary conditions
 u_x0 = fb;
 u_xb = gb;
 u_y0 = fb(1)+(x/b).*(gb(1)-fb(1));
-%Build the vector for U values
-U = zeros((n-1)^2,1);
-U_D = ones(n-1,1); %assign a value for the Dirchielt boundary conditions
 
-%ghost node
-z=1;
-for i = h:h:b-h
-    F_D(z) = sin(pi*i/b)*cos(pi/2);
-    z=z+1;
+for j = 1:n
+    for i = 1:n-1
+        if j == 1 && i == 1
+            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*u_x0(j);
+        elseif j ==1 && i < n-1
+            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i+1)+U_n(j,i-1))+(h^2/4)*F(j,i);
+        elseif j == 1 && i == n-1
+            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_xb(j);
+        elseif j > 1 && j < n && i == 1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*u_x0(j);
+        elseif j > 1 && j < n && i < n-1 && i >1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1))+(h^2/4)*F(j,i);
+        elseif j > 1 && j < n && i == n-1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_xb(j);
+        elseif j == n && i == 1
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*(u_x0(j)+u_y0(i+1));
+        elseif j == n && i > 1 && i < n-1
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_y0(i+1);
+        else
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*(u_xb(j)+u_y0(i+1));
+        end
+    end
 end
 
