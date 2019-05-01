6ee16741f0792a698a5beec910e9d803b9798bf5
chixon10398 <49700356+chixon10398@users.noreply.github.com>
2019-04-30 21:43:14 -0500

    Update project.m
    fixed the loop. removed the ghost nodes from the code. need to improve for loop to improve performance.

--- project.m
+++ project.m
@@ -6,20 +6,25 @@ clc
 a = 0;
 b = 2*pi;
 %discretize the problem
-n = 6; %number of steps to b
+n = 10; %number of steps to b
 h = b/n; %distance between each position
 x = 0:h:b;
 y = x;
+%tolerance for acceptable value
+tol = 10^-5;
 %create the functions
-fb = y.*(b-y);
+fb = y.*(b-y).^2;
 gb = ((b-y).^2).*cos(pi*y/b);
-F = zeros(n,n-1); %create vector for F values
-U_n = zeros(n,n-1); %U for n
-U_n1 = zeros(n,n-1); % U for n+1
+F = zeros(n-1,n-1); %create vector for F values
+U_n = zeros(n-1,n-1); %U for n
+U_n1 = zeros(n-1,n-1); % U for n+1
+save('variables','U_n','U_n1')
+load('variables','U_n','U_n1')
+ghost = zeros(1,n-1); %wher the values for our ghost node will be stored
 %create the elements for F
-for j = 1:n
-    for i = 1:n-1
-        F(j,i) = sin(pi*i/b)*cos((pi/2)*(2*j/b+1));
+for i = 1:n-1
+    for j = 1:n-1
+        F(j,i) = sin(pi*((x(i+1)-a)/(b-a)))*cos((pi/2)*((2*((y(j+1)-a)/(b-a)))+1));
     end
 end
 
@@ -28,27 +33,47 @@ u_x0 = fb;
 u_xb = gb;
 u_y0 = fb(1)+(x/b).*(gb(1)-fb(1));
 
-for j = 1:n
-    for i = 1:n-1
-        if j == 1 && i == 1
-            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*u_x0(j);
-        elseif j ==1 && i < n-1
-            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i+1)+U_n(j,i-1))+(h^2/4)*F(j,i);
-        elseif j == 1 && i == n-1
-            U_n1(j,i) = (1/4)*(2*U_n(j+1,i)+U_n(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_xb(j);
-        elseif j > 1 && j < n && i == 1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*u_x0(j);
-        elseif j > 1 && j < n && i < n-1 && i >1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1))+(h^2/4)*F(j,i);
-        elseif j > 1 && j < n && i == n-1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_xb(j);
-        elseif j == n && i == 1
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1))+(h^2/4)*F(j,i)-(1/4)*(u_x0(j)+u_y0(i+1));
-        elseif j == n && i > 1 && i < n-1
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*u_y0(i+1);
-        else
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n1(j,i-1))+(h^2/4)*F(j,i)-(1/4)*(u_xb(j)+u_y0(i+1));
-        end
+%ghost node
+for i = 1:n-1
+    if i == 1
+        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i+1,1)+h^2*F(i,1)+u_x0(i));
+    elseif i == n-1
+        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i-1,1)+h^2*F(i,j)+u_xb(i));
+    else
+        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i+1,1)+U_n(i-1,1)+h^2*F(j,i));
     end
 end
 
+%Iterate for the next values
+index = 1;
+diff = 1;
+while diff > tol
+for j = 1:n-1
+    for i = 1:n-1
+        if j == 1 && i == 1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n(j,i+1)+h^2*F(j,i)+ghost(i)+u_x0(j+1));
+        elseif j == 1 && i == n-1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i) + u_xb(j+1));
+        elseif j == n-1 && i == 1
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+h^2*F(j,i)+u_x0(j+1)+u_y0(i+1));
+        elseif j == n-1 && i == n-1
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1)+u_y0(i+1));
+        elseif j == 1 && i > 1 && i < n-1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i));
+        elseif j == n-1 && i > 1 && i < n-1
+            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+u_y0(i+1));
+        elseif i == 1 && j > 1 && j < n-1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+h^2*F(j,i)+u_x0(j+1));
+        elseif i == n-1 && j > 1 && j < n-1
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1));
+        else            
+            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i));
+        end 
+        save('variables','U_n1')
+    end
+end
+diff = norm(U_n1-U_n);
+U_n = U_n1;
+index = index+1
+save('variables','U_n')
+end
