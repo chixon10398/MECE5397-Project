e416c405c9da85a204f2540491ce00ea9965621d
chixon10398 <49700356+chixon10398@users.noreply.github.com>
2019-05-01 23:48:08 -0500

    Update project.m
    working on the optimization of code. much faster now.

--- project.m
+++ project.m
@@ -6,10 +6,10 @@ clc
 a = 0;
 b = 2*pi;
 %discretize the problem
-n = 10; %number of steps to b
+n = 100; %number of steps to b
 h = b/n; %distance between each position
 x = 0:h:b;
-y = x;
+y = b:-h:0;
 %tolerance for acceptable value
 tol = 10^-5;
 %create the functions
@@ -20,7 +20,8 @@ U_n = zeros(n-1,n-1); %U for n
 U_n1 = zeros(n-1,n-1); % U for n+1
 save('variables','U_n','U_n1')
 load('variables','U_n','U_n1')
-ghost = zeros(1,n-1); %wher the values for our ghost node will be stored
+ghost = zeros(1,n-1);
+g = ghost;
 %create the elements for F
 for i = 1:n-1
     for j = 1:n-1
@@ -31,47 +32,46 @@ end
 %Boundary conditions
 u_x0 = fb;
 u_xb = gb;
-u_y0 = fb(1)+(x/b).*(gb(1)-fb(1));
+u_y0 = fb(n+1)+(x/b).*(gb(n+1)-fb(n+1));
+
 
 %ghost node
-for i = 1:n-1
-    if i == 1
-        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i+1,1)+h^2*F(i,1)+u_x0(i));
-    elseif i == n-1
-        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i-1,1)+h^2*F(i,j)+u_xb(i));
-    else
-        ghost(i) = (1/4)*(2*U_n(i,2)+U_n(i+1,1)+U_n(i-1,1)+h^2*F(j,i));
-    end
-end
 
-%Iterate for the next values
 index = 1;
 diff = 1;
 while diff > tol
-for j = 1:n-1
-    for i = 1:n-1
-        if j == 1 && i == 1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n(j,i+1)+h^2*F(j,i)+ghost(i)+u_x0(j+1));
-        elseif j == 1 && i == n-1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i) + u_xb(j+1));
-        elseif j == n-1 && i == 1
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+h^2*F(j,i)+u_x0(j+1)+u_y0(i+1));
-        elseif j == n-1 && i == n-1
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1)+u_y0(i+1));
-        elseif j == 1 && i > 1 && i < n-1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i));
-        elseif j == n-1 && i > 1 && i < n-1
-            U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+u_y0(i+1));
-        elseif i == 1 && j > 1 && j < n-1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+h^2*F(j,i)+u_x0(j+1));
-        elseif i == n-1 && j > 1 && j < n-1
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1));
-        else            
-            U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i));
-        end 
-        save('variables','U_n1')
+    for i = 1:n-1 %this loop adds the ghost boundarys
+        if i == 1
+            ghost(i) = (1/4)*(2*U_n(1,i)+g(i+1)+u_x0(n+1));
+            U_n1(1,i) = (1/4)*(U_n(2,i)+U_n(1,i+1)+h^2*F(1,i)+ghost(i)+u_x0(2));
+        elseif i == n-1
+            ghost(i) = (1/4)*(2*U_n(1,i)+g(i-1)+u_xb(n+1));
+            U_n1(1,i) = (1/4)*(U_n(2,i)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i)+u_xb(2));
+        else
+            ghost(i) = (1/4)*(2*U_n(1,i)+g(i+1)+g(i-1));
+            U_n1(1,i) = (1/4)*(U_n(2,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+ghost(i));
+        end
     end
-end
+    g = ghost;
+    for j = 2:n-1
+        for i = 2:n-1
+            if j == n-1 && i == n-1
+                U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1)+u_y0(i+1));
+            elseif j == n-1 && i < n-1
+                U_n1(j,i) = (1/4)*(U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i)+u_y0(i+1));
+            elseif j < n-1 && i == n-1
+                U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n1(j,i-1)+h^2*F(j,i)+u_xb(j+1));
+            elseif j < n-1 && i < n-1
+                U_n1(j,i) = (1/4)*(U_n(j+1,i)+U_n1(j-1,i)+U_n(j,i+1)+U_n1(j,i-1)+h^2*F(j,i));
+            end
+        end
+        if j == n-1
+            U_n1(j,1) = (1/4)*(U_n1(j-1,1)+U_n(j,2)+h^2*F(j,1)+u_x0(j+1)+u_y0(2));
+        elseif j < n-1
+            U_n1(j,1) = (1/4)*(U_n(j+1,1)+U_n1(j-1,1)+U_n(j,2)+h^2*F(j,i)+u_x0(j+1));
+        end
+    end
+save('variables','U_n1')
 diff = norm(U_n1-U_n);
 U_n = U_n1;
 index = index+1
